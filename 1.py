f072794ea5fda174abb13ced9aa860776d272de2
nikhil7913 <47119554+nikhil7913@users.noreply.github.com>
2019-01-30 19:41:36 +0000

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-import os
-start_folder = '/home'
-for dirName, subdirList, fileList in os.walk(start_folder):
-    print('Found directory: %s' % dirName)
-    for fname in fileList:
-        print('\t%s' % fname)
