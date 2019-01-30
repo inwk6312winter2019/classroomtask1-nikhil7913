87cf02279053343aba25437bd5032a27e609f02b
sai <sh354053@dal.ca>
2019-01-30 11:38:02 -0800

    third

--- 1.py
+++ 1.py
@@ -1,7 +1,6 @@
 import os
- 
-rootDir = '/home'
-for dirName, subdirList, fileList in os.walk(rootDir):
+start_folder = '/home'
+for dirName, subdirList, fileList in os.walk(start_folder):
     print('Found directory: %s' % dirName)
     for fname in fileList:
         print('\t%s' % fname)
