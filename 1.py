4eadb36407c105affcf966ae29676605ec9f6cd5
sai <sh354053@dal.ca>
2019-01-30 11:17:00 -0800

    second commit

--- 1.py
+++ 1.py
@@ -1,1 +1,7 @@
-print("acb")
+import os
+ 
+rootDir = '/home'
+for dirName, subdirList, fileList in os.walk(rootDir):
+    print('Found directory: %s' % dirName)
+    for fname in fileList:
+        print('\t%s' % fname)
